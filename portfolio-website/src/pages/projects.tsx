9a422ba1ea8f5c4e6496c688c99435981590a2a1
n29lodg <n29lodg@siena.edu>
2025-05-08 16:21:15 -0400

    Added some projects with link to github, committing so i can change devices

--- portfolio-website/src/pages/projects.tsx
+++ portfolio-website/src/pages/projects.tsx
@@ -10,7 +10,11 @@ function Projects() {
       <h1>Projects</h1>
       <p>Here are some of my projects.</p>
       <ul>
-        <li>Koch Curves (in school)</li>
+        <li><a href='https://github.com/csis225f24/ps4-n29lodg'>Koch Curves (in school)</a></li>
+        <ul>
+            <li>Uses Java Swing for graphics</li>
+            <li>Draw a line, use button to advance to the next stage in the koch curve fractal <a href='https://www.geeksforgeeks.org/koch-curve-koch-snowflake/'>(Click here to learn more about the koch snowflake)</a></li>
+        </ul>
         <li>Software engineering in school project (secret)</li>
         <li>Media vault (personal project, work in progress)</li>
       </ul>
