cfced92cf3c683b72c935b68f155fa102fa77381
n29lodg <n29lodg@siena.edu>
2025-05-08 22:04:37 -0400

    small changes

--- portfolio-website/src/pages/skills.tsx
+++ portfolio-website/src/pages/skills.tsx
@@ -11,23 +11,40 @@ function Skills() {
         put into two categories, proficient, and familiar. Proficient in my eyes means that I have strong
         knowledge in that area, while familiar to me means that I understand what the technology is used for, but would need further learning or help to apply it effectively.
       </p>
+      
+      <div id='info-container'>
+        <div id='skills-list'>
+            <p>Proficient:</p>
+            <ul>
+                <li>Java</li>
+                <li>Python</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Figma (With certificate)</li>
+                <li>Oracle SQL</li>
+            </ul>
 
-      <p>Proficient:</p>
-      <ul>
-        <li>Java</li>
-        <li>Python</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Figma (With certificate)</li>
-        <li>Oracle SQL</li>
-      </ul>
-
-      <p>Familiar:</p>
-      <ul>
-        <li>React</li>
-        <li>Java Spring Boot</li>
-        <li>Mongo DB noSQL</li>
-      </ul>
+            <p>Familiar:</p>
+            <ul>
+                <li>React</li>
+                <li>Java Spring Boot</li>
+                <li>Mongo DB noSQL</li>
+            </ul>
+        </div>
+        <div id='courses-list'>
+            <p>Courses taken:</p>
+            <ul>
+                <li>Data Structures</li>
+                <li>Algorithms</li>
+                <li>Software Development</li>
+                <li>Database Management Systems</li>
+                <li>Web Development</li>
+                <li>Discrete Math</li>
+                <li>Theory of Computation</li>
+                <li>Machine Learning</li>
+            </ul>
+        </div>
+        </div>
     </div>
   );
 }
