28451828c74771b848eab1b17eeeec81c9dd6e81
n29lodg <n29lodg@siean.edu>
2025-05-09 17:05:59 -0400

    Added content to home page, working on layout

--- portfolio-website/src/pages/home.tsx
+++ portfolio-website/src/pages/home.tsx
@@ -6,25 +6,27 @@ function Home() {
     return(
         <div className="home-main-container">
             <NavBar />
-            <div className="page-contents">
-                <div className="contents-container">
+            <div id="page-contents">
+                <div id="contents-container" className="box">
                     <div id="profile-card" className="box">
-
+                        <p></p>
                     </div>
                 </div>
 
-                <div id="information">
+                <div id="information" className="flex">
                     <div id="about-me" className="box">
-                        <p>My name is Nicholas Lodge, and I'm a 21 year old computer science and data science dual major graduand at Siena College in Albany, New York.
+                        <p>
+                            My name is Nicholas Lodge, and I'm a 21 year old computer science and data science dual major graduand at Siena College in Albany, New York.
                             I came into school as a major in actuarial science before realizing that wasn't for me. I then changed into sports communications
                             as I love sports, but quickly realized that even moreso wasn't for me. I ended up trying data science and computer science collectively in the spring of my sophomore year,
                             and quickly found that I love the field. I enjoy learning to code and am very eager to learn more as I have tons of room for growth and improvement.
-                            I'm newer to the field compared to my peers as I didn't write my first lines of code until I was nearly 20 years old, but I share an equal desire to build software that helps people, and to make an impact in some way.
+                            
                         </p>
                     </div>
-                    <div className="box">
+                    <div id="interests" className="box">
                         <p>
-                            Some of my interests and hobbies include
+                            Some of my interests and hobbies include sports, video games, and music. I'm a big NBA fan and love the Boston Celtics. I also like soccer and I support AC Milan. 
+                            I like to play a variety of video games such as Minecraft, Pokemon, FIFA, and a further variety of RPG's. Lastly, I enjoy listening to music all the time for any given reason, 
                         </p>
                     </div>
                 </div>
