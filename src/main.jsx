e31ec5ebc40fca4d79b115831b5438ddeb2a8ace
n29lodg <n29lodg@siean.edu>
2025-05-09 20:42:28 -0400

    another test

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+    <App />
+  /* </StrictMode>, */
+)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
