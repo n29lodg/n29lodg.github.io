2ea5879116112b548bd4b62c702567062512a881
n29lodg <n29lodg@siean.edu>
2025-05-09 22:23:55 -0400

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import React from 'react'
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 import Projects from './pages/projects'
 import Skills from './pages/skills'
@@ -9,17 +10,12 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter basename='/website'>
-      <body>
-        {/* <NavBar /> */}
-      </body>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
         {/* <Route path="/resume" element={<Resume />} /> */}
       </Routes>
-    </BrowserRouter>
   )
 }
 
